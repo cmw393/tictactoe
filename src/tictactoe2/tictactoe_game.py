d75cf92a93b138c9a8631ae19dcf7effd7276245
Collin Wilkinson <cmw393@drexel.edu>
2023-10-20 12:08:17 -0400

    edits on tictactoe game to improve its percent

--- src/tictactoe2/tictactoe_game.py
+++ src/tictactoe2/tictactoe_game.py
@@ -2,7 +2,7 @@ def print_board(board):
     for row in board:
         print(" | ".join(row))
         print("-" * 9)
-
+    
 def check_win(board, player):
     for row in board:
         if all(cell == player for cell in row):
